c993735ce3e8f8d61f652b70c69b0e83d07fee23
Matheus Manzutti <mateusmanzutti@gmail.com>
2021-12-25 21:04:39 -0300

    End chapter 1 - CRUD

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cristiano', '125.653.896', 2200.0, TIMESTAMP WITHOUT TIME ZONE '1988-09-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roberto', '485.963.111', 5200.0, TIMESTAMP WITHOUT TIME ZONE '1998-08-25T03:43:00.45689Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Hulgo', '785.145.159', 6500.0, TIMESTAMP WITHOUT TIME ZONE '1985-04-11T11:26:15.12365Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Richard', '565.698.633', 2500.0, TIMESTAMP WITHOUT TIME ZONE '1978-05-05T15:35:35.78932Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Robson', '951.356.357', 5500.0, TIMESTAMP WITHOUT TIME ZONE '1967-05-24T23:26:51.75842Z', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Gabriela', '459.852.211', 4600.0, TIMESTAMP WITHOUT TIME ZONE '1999-01-19T16:54:25.14785Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Francis', '145.896.265', 6600.0, TIMESTAMP WITHOUT TIME ZONE '1978-03-31T13:53:13.52369Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joseane', '235.988.444', 7800.0, TIMESTAMP WITHOUT TIME ZONE '1995-06-14T19:20:08.85246Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rebeca', '235.598.112', 1000.0, TIMESTAMP WITHOUT TIME ZONE '1992-02-06T22:44:38.78641Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Naiara', '439.614.852', 3000.0, TIMESTAMP WITHOUT TIME ZONE '1985-06-21T12:22:48.34521Z', 2);
